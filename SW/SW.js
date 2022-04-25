bafab8cfebfb875a3260f2ba1e3b431bc8e9189e
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-04-25 15:09:05 +0200

    Add files via upload

--- SW/SW.js
+++ SW/SW.js
@@ -1,3 +1,25 @@
 "use strict";
 
-function
+function obtenerPeliculas(id){
+    let URL = 'https://swapi.dev/api/films/${id}'
+    return fetch(URL)
+        .then(r => r.json())
+}
+
+function obtenerPersonaje(id){
+    let URL = 'https://swapi.dev/api/people/${id}'
+    return fetch(URL)
+        .then(r => r.json())
+}
+
+function obtenerStarship(id){
+    let URL = 'https://swapi.dev/api/starships/${id}'
+    return fetch(URL)
+        .then(r => r.json())
+}
+
+function obtenerVehicles(id){
+    let URL = 'https://swapi.dev/api/vehicles/${id}'
+    return fetch(URL)
+        .then(r => r.json())
+}
